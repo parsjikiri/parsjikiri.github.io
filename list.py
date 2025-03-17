6b788498ffac2e9c4f3e91e235ffaeca70f440be
parsjikiri <parsjikiri@gmail.com>
2025-03-17 17:35:55 +0800

    Sets

---
+++ list.py
@@ -0,0 +1,13 @@
+#Define list of names
+names= ["harry","ron","hermione","ginny"]
+
+print(names)
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+names.append("draco")
+
+names.sort()
+print(names)
