02b78d0ff56b9ec0f8686b87d937dd3461cb4d23
parsjikiri <parsjikiri@gmail.com>
2025-04-03 18:49:37 +0800

    new learnings

--- sets.py
+++ sets.py
@@ -6,7 +6,11 @@ set.add(1)
 set.add(2)
 set.add(3)
 set.add(4)
-set.add(3) # no duplicates
+set.add(3) # no duplicates will appear on set
 set.remove(1)# to remove
+print(set)
+print(f"The elements has {len(set)} parts")
+set.update({4,2,3,6}) # add multiple elements
+set.add(89) # add set inside set
 print(set)
 print(f"The elements has {len(set)} parts")
