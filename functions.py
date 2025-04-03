cbf74905f9be0a1f58681da9a8c5e2eb7e498791
parsjikiri <parsjikiri@gmail.com>
2025-04-03 19:58:53 +0800

    adding functions

---
+++ functions.py
@@ -0,0 +1,14 @@
+def square(x):
+    return x*x
+
+x = int(input("Enter x: "))
+print(square(x))
+
+def rectangle(w,l):
+    return w*l
+
+w = int(input("Enter Widht: "))
+l = int(input("Enter Length: "))
+print(rectangle(w,l))
+
+
