893d4c1ebbf8643306a325584e0fb98942c93d33
parsjikiri <parsjikiri@gmail.com>
2025-04-03 18:52:51 +0800

    pushing remaining

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,13 @@
 houses = {"harry":"gryffindor","ron":"gryffindor","hermione":"gryffindor","draco":"slytherin","snape":"slytherin"}
+
 print(houses["harry"])
 print(houses["ron"])
+print(houses)
+houses["pars"] = "slytherin"
+print(houses)
+name = input("Enter name: ")
+print(houses[name])
+dname = input("Enter dname: ")
+dhouses = input("Enter dhouses: ")
+houses.update({dname: dhouses})
 print(houses)
--- loops.py
+++ loops.py
@@ -1,3 +1,14 @@
 for i in [0,1,2,3,4,5]:
     print(i)
     print(f"count is {(i)}, and here is the content {i}")
+
+for j in range(6):
+    print(j)
+
+names = ['harry','ron','hermione','ginny']
+for name in names:
+    print(name)
+
+character = "harry"
+for char in character:
+    print(char)
